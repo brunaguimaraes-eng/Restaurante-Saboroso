be2812d87f1b77bfc9421a21e41978985aadf2f5
Bruna Guimaraes <bruna.guimaraes@saipos.com>
2026-06-10 00:38:20 -0300

    MY20 - Exibindo dados do usuário autenticado

--- routes/admin.js
+++ routes/admin.js
@@ -26,12 +26,8 @@ router.get("/logout", function(req, res, next){
     res.redirect("admin/login");
 });
 
-router.get("/", function(req, res, next){
-    
-    res.render("admin/index", {
-        menus:req.menus
-    });
-
+router.get("/", function(req, res, next){    
+    res.render("admin/index", admin.getParams(req));
 });
 
 router.get("/login", function(req, res, next){
@@ -61,41 +57,32 @@ router.post("/login", function(req, res, next){
 
 router.get("/contacts", function(req, res, next){
 
-    res.render("admin/contacts",{
-        menus:req.menus
-    });
+    res.render("admin/contacts", admin.getParams(req));
 
 })
 
 router.get("/emails", function(req, res, next){
 
-    res.render("admin/emails",{
-        menus:req.menus
-    });
+    res.render("admin/emails", admin.getParams(req));
 
 })
 
 router.get("/menus", function(req, res, next){
 
-    res.render("admin/menus",{
-        menus:req.menus
-    }); 
+    res.render("admin/menus", admin.getParams(req));
 
 })
 
 router.get("/reservations", function(req, res, next){
 
-    res.render("admin/reservations", {
-        date:{},
-        menus: req.menus
-    });
+    res.render("admin/reservations", admin.getParams(req, {
+        date: {}
+    }));
 });
 
 router.get("/users", function(req, res, next){
 
-    res.render("admin/users",{
-        menus:req.menus
-    });  
+    res.render("admin/users", admin.getParams(req));
 
 })
 
