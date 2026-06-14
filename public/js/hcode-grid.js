915334148e0a227bfbe98e3434586c794368e29e
Bruna Guimaraes <bruna.guimaraes@saipos.com>
2026-06-13 21:15:45 -0300

    W36 - eventos do componente

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -2,6 +2,12 @@ class HcodeGrid {
 
     constructor(configs) {
 
+        configs.listeners = Object.assign({
+            afterUpdateClick: (e) =>{
+                $('#modal-update').modal('show');
+            }, 
+        }, configs.listeners);
+
        this.options = Object.assign({}, {
         formCreate: '#modal-create form',
         formUpdate: '#modal-update form',
@@ -35,11 +41,20 @@ class HcodeGrid {
 
     }
 
+    fireEvent(name, args){
+
+        if (typeof this.options.listeners[name] === 'function') this.options.listeners[name].apply(this, args);
+
+
+    }
+
     initButtons(){
 
         [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
 
             btn.addEventListener('click', e => {
+
+                this.fireEvent('beforeUpdateClick', [e]);
 
                 let tr = e.composedPath().find(el => {
                     return(el.tagName.toUpperCase() === 'TR');
@@ -61,7 +76,7 @@ class HcodeGrid {
 
                 }
 
-                $('#modal-update').modal('show');
+                this.fireEvent('afterUpdateClick', [e]);
 
             })
 
